9f4beead67e15bb260b9c420389220963d67885c
gipark <gipark181@gmail.com>
2022-05-06 07:30:45 +0900

    상품 수정 기능

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.shop.controller;
 import com.shop.dto.ItemFormDTO;
 import com.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -63,5 +64,27 @@ public class ItemController {
         }
 
         return "item/itemForm";
+    }
+
+    @PostMapping(value = "/admin/item/{itemId}")
+    public String itemUpdate(@Valid ItemFormDTO itemFormDTO, BindingResult bindingResult, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "item/itemForm";
+        }
+
+        if (itemImgFileList.get(0).isEmpty() && itemFormDTO.getId() == null) {
+            model.addAttribute("errorMessage", "첫 번째 상품 이미지는 필수 입력 값 입니다.");
+            return "item/itemForm";
+        }
+
+        try {
+            itemService.updateItem(itemFormDTO, itemImgFileList);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "상품 수정 중 에러가 발생하였습니다.");
+            return "item/itemForm";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -62,4 +62,21 @@ public class ItemService {
 
         return itemFormDTO;
     }
+
+    public Long updateItem(ItemFormDTO itemFormDTO, List<MultipartFile> itemImgFileList) throws Exception {
+
+        // 상품 수정
+        Item item = itemRepository.findById(itemFormDTO.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDTO);
+
+        List<Long> itemImgIds = itemFormDTO.getItemImgIds();
+
+        // 이미지 등록
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
 }
